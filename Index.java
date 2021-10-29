e4e8a5a08220bd015f800ea11d866ae0318c2d24
kaviarasanR <91777015+kaviarasanR@users.noreply.github.com>
2021-10-30 02:35:47 +0530

    Add files via upload

---
+++ Index.java
@@ -0,0 +1,14 @@
+package week3.day1;
+
+public class Index {
+
+	public static void main(String[] args) {
+		String Text = "Java Excercise";
+		int place = Text.indexOf("E");
+		System.out.println(place);
+		int place2 = Text.indexOf("s");
+		System.out.println(place2);
+
+	}
+
+}
