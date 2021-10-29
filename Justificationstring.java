e2bd9243521254870201d5b3cc630d72c7d977df
kaviarasanR <91777015+kaviarasanR@users.noreply.github.com>
2021-10-30 02:17:50 +0530

    Add files via upload

---
+++ Justificationstring.java
@@ -0,0 +1,17 @@
+package week3.day1;
+
+public class Justificationstring {
+
+	public static void main(String[] args) {
+		String str3 = new String("Kutty");
+		 String str4 = new String("Kutty");
+		 
+		 if(str3==str4) {
+			 System.out.println(" Same text");
+		 }
+		 else
+			 System.out.println("Diff text");
+		 //memory location has been compared here so it is giving "Diff text"
+	}
+
+}
