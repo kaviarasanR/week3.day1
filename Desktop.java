4facb740f0284ebfa2c0adb0e19fa7245b993061
kaviarasanR <91777015+kaviarasanR@users.noreply.github.com>
2021-10-29 19:35:38 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,13 @@
+package org.system;
+
+public class Desktop extends Computer {
+	public void desktopSize() {
+   }
+	public static void main(String[] args) {
+		Desktop method=new Desktop();
+		method.computerModel();
+		method.desktopSize();
+		
+	}
+
+}
