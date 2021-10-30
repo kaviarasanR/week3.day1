e64c315f2412b6818816c2b367ed2132e4eaa91b
kaviarasanR <91777015+kaviarasanR@users.noreply.github.com>
2021-10-30 14:32:38 +0530

    Add files via upload

---
+++ Replace.java
@@ -0,0 +1,16 @@
+package week3.day1;
+
+public class Replace {
+
+	public static void main(String[] args) {
+		String sentence = "I am working with Java8";
+		;
+       System.out.println(sentence.replace("8", "11")); 
+       for (int i =5; i < sentence.length()-8; i++) {
+		System.out.println(sentence.charAt(i));
+	}
+				
+       
+	}
+
+}
